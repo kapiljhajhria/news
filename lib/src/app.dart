322f8e41d15057f5df1fe13409fbab7c6b504a70
kapil jhajhria <jhajhria44@gmail.com>
2021-01-30 21:42:06 +0530

    init struct for app

---
+++ lib/src/app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:news/src/screens/home_screen.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
