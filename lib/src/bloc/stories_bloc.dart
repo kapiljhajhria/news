5ab6b14c4617716c8f599eb12f19627655fcedb4
kapil jhajhria <jhajhria44@gmail.com>
2021-02-07 17:07:46 +0530

    refactored stories bloc

--- lib/src/bloc/stories_bloc.dart
+++ lib/src/bloc/stories_bloc.dart
@@ -5,19 +5,31 @@ import '../models/item_model.dart';
 import '../resources/repository.dart';
 
 class StoriesBloc {
-  final _topIds = PublishSubject<List<int>>(onListen: () {});
-  final _items = BehaviorSubject<int>();
   final _repository = Respository();
+  final _topIds = PublishSubject<List<int>>();
+  final _itemsOutput = BehaviorSubject<Map<int, Future<ItemModel>>>();
+  final _itemsFetcher = PublishSubject<int>();
 
   // Observable<Map<int,Future<ItemModel>>> items;
-  Stream<Map<int, Future<ItemModel>>> items;
+  // Stream<Map<int, Future<ItemModel>>> items; //no longer needed
+
+  //getters to streams
+
   //getter to get stream/ rxdart = observable -depreciated
-  get topIds => _topIds.stream; //stream with no data right now
+  Stream<List<int>> get topIds =>
+      _topIds.stream; //stream with no data right now
+
+  Stream<Map<int, Future<ItemModel>>> get items => _itemsOutput.stream;
 
   // get items => _items.stream.transform(_itemsTransformer());
 
+  //getter to sinks
+  Function(int) get fetchItem => _itemsFetcher.sink.add;
+
   StoriesBloc() {
-    items = _items.stream.transform(_itemsTransformer());
+    // items = _items.stream.transform(_itemsTransformer());
+
+    _itemsFetcher.stream.transform(_itemsTransformer()).pipe(_itemsOutput);
   }
 
   fetchTopIds() async {
@@ -28,20 +40,22 @@ class StoriesBloc {
     _topIds.sink.add(ids);
   }
 
-  Function(int) get fetchItem => _items.sink.add;
-
   _itemsTransformer() {
     print("itemTrasformer called");
     return ScanStreamTransformer(
-        (Map<int, Future<ItemModel>> cache, int id, index) {
-      print("index $index");
-      cache[id] = _repository.fetchItem(id);
-      return cache;
-    }, <int, Future<ItemModel>>{});
+      (Map<int, Future<ItemModel>> cache, int id, index) {
+        print("index $index");
+        cache[id] = _repository.fetchItem(id);
+        return cache;
+      },
+      <int, Future<ItemModel>>{},
+    );
   }
 
   dispose() {
+    _itemsOutput.close();
+    _itemsFetcher.close();
     _topIds.close();
-    _items.close();
+    // _items.close();
   }
 }
