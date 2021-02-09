2c23993ebbb79318ed341f0470166efb813f919a
kapil jhajhria <jhajhria44@gmail.com>
2021-02-09 15:46:37 +0530

    comments bloc fetching all comments on visiting comments screen

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -2,12 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:news/src/screens/news_details_screen.dart';
 import 'screens/newslist_screen.dart';
 import 'bloc/stories_provider.dart';
+import 'bloc/comments_provider.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return StoriesProvider(
-      child: MaterialApp(title: "Awesome News", onGenerateRoute: routes),
+    return CommentsProvider(
+      child: StoriesProvider(
+        child: MaterialApp(title: "Awesome News", onGenerateRoute: routes),
+      ),
     );
   }
 
@@ -17,9 +20,11 @@ class App extends StatelessWidget {
         return NewsList();
       });
     }
-    final itemId = int.parse(settings.name.replaceFirst("/", ""));
 
     return MaterialPageRoute(builder: (context) {
+      final itemId = int.parse(settings.name.replaceFirst("/", ""));
+      final commentsBloc = CommentsProvider.of(context);
+      commentsBloc.fetchItemWithComments(itemId);
       return NewsDetails(itemId: itemId);
     });
   }
--- lib/src/bloc/comments_bloc.dart
+++ lib/src/bloc/comments_bloc.dart
@@ -1,1 +1,43 @@
-class CommentsBloc {}
+import 'dart:async';
+import '../models/item_model.dart';
+import '../resources/repository.dart';
+import 'package:rxdart/rxdart.dart';
+
+class CommentsBloc {
+  final _repository = Repository();
+  final _commentsFetcher = PublishSubject<int>();
+  final _commentsOutput = BehaviorSubject<Map<int, Future<ItemModel>>>();
+
+  //Stream
+  Stream<Map<int, Future<ItemModel>>> get itemWithComments =>
+      _commentsOutput.stream;
+
+  //Sink
+  Function(int) get fetchItemWithComments => _commentsFetcher.sink.add;
+
+  CommentsBloc() {
+    //init commentsFetcher and pipe it to output
+    _commentsFetcher.stream
+        .transform(_commentsTransformer())
+        .pipe(_commentsOutput);
+  }
+
+  StreamTransformer<int, Map<int, Future<ItemModel>>> _commentsTransformer() {
+    return ScanStreamTransformer<int, Map<int, Future<ItemModel>>>(
+        (Map<int, Future<ItemModel>> cache, int id, index) {
+      print("comments index no $index");
+      cache[id] = _repository.fetchItem(id);
+      cache[id].then((ItemModel item) {
+        item.kids.forEach((kidId) {
+          fetchItemWithComments(kidId);
+        });
+      });
+      return cache;
+    }, <int, Future<ItemModel>>{});
+  }
+
+  dispose() {
+    _commentsFetcher.close();
+    _commentsOutput.close();
+  }
+}
