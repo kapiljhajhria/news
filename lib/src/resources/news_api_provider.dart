56a486d3bc99a59618359b32954a70ee135a8f85
kapil jhajhria <jhajhria44@gmail.com>
2021-03-04 10:50:11 +0530

    updated for flutter v2

--- lib/src/resources/news_api_provider.dart
+++ lib/src/resources/news_api_provider.dart
@@ -10,13 +10,13 @@ class NewsApiProvider implements Source {
   String _rootUrl = "https://hacker-news.firebaseio.com/v0";
 
   Future<List<int>> fetchTopIds() async {
-    final response = await client.get("$_rootUrl/topstories.json");
+    final response = await client.get(Uri.parse("$_rootUrl/topstories.json"));
     final ids = json.decode(response.body);
     return ids.cast<int>();
   }
 
   Future<ItemModel> fetchItem(int id) async {
-    final response = await client.get("$_rootUrl/item/$id.json");
+    final response = await client.get(Uri.parse("$_rootUrl/item/$id.json"));
     final parsedJson = json.decode(response.body);
     return ItemModel.fromJson(parsedJson);
   }
